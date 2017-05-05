39f6bb447c665fcc1b2d4981b492ff30cdccac79
Magno Gouveia <owlflee@gmail.com>
2017-05-05 11:33:00 -0300

    Removed pokemon routes from index

--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@
 
 const express = require('express'),
       app = express(),
-      bodyParser = require('body-parser');
+      bodyParser = require('body-parser'),
+      pokemons = require('./routes/pokemons');
 
 app.use(bodyParser.json());
+
+app.use('/pokemons',pokemons)
 
 module.exports = app;
